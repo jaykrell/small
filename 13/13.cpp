4bbabf2974b06f462f81728a057a0e867603986a
JayKrell <jay.krell@cornell.edu>
2021-04-09 16:41:22 -0700

    tweak

--- 13/13.cpp
+++ 13/13.cpp
@@ -2,11 +2,19 @@
 
 #include <windows.h>
 
-char windowProc;
-
+__declspec(safebuffers)
 LRESULT __stdcall Entry(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    if (windowProc)
+    char hello[] = "hello";
+    WNDCLASS wc = {0,Entry,0,0,0,0,0,(HBRUSH)1,0,hello};
+    if (RegisterClass(&wc))
+    {
+        ShowWindow(CreateWindowEx(0,hello,hello,WS_OVERLAPPEDWINDOW,0,0,0,0,0,0,0,0), SW_MAXIMIZE);
+        MSG msg;
+        while (GetMessage(&msg, NULL, 0, 0))
+            DispatchMessage(&msg);
+    }
+    else
     {
         switch (uMsg)
         {
@@ -14,14 +22,5 @@ LRESULT __stdcall Entry(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-    windowProc = 1;
-    static const char hello[]  = "hello";
-    WNDCLASS wc = {0,Entry,0,0,0,0,0,(HBRUSH)1,0,hello};
-    RegisterClass(&wc);
-    ShowWindow(CreateWindowEx(0,hello,hello,WS_OVERLAPPEDWINDOW,0,0,0,0,0,0,0,0), SW_MAXIMIZE);
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-        DispatchMessage(&msg);
-
     return 0;
 }
